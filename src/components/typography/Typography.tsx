1194f64eba9ac473b10b375aed83bcf20809fa7e
Armen Mesropyan <armenbangits@gmail.com>
2021-09-06 17:24:24 +0400

    Added typography-sizes function

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import styles from './Typography.module.scss';
 
 const Typography = ({ children }) => {
-  return <div>{children}</div>;
+  return <div className={`${styles.Typography} ${styles.h1}`}>{children}</div>;
 };
 
 export default Typography;

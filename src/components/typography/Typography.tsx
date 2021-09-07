af41322781626f97f224c78e1ae3ed9941cb1f43
ShavarshMartirosyan <shavarshmartirosyan.bangits@gmail.com>
2021-09-07 18:46:09 +0400

    Created Typography component with dynamic component and variant changing

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -1,7 +1,19 @@
+import { FC, JSXElementConstructor } from 'react';
 import styles from './Typography.module.scss';
+import classNames from 'classnames';
 
-const Typography = ({ children }) => {
-  return <div className={`${styles.Typography} ${styles.p1}`}>{children}</div>;
+export type TypographyVariants = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p1' | 'p2' | 'p3' | 'p4';
+
+export type TypographyProps = {
+  variant?: TypographyVariants;
+  component?: string | JSXElementConstructor<any>;
+  className?: string;
+};
+
+const Typography: FC<TypographyProps> = ({ children, variant = 'p1', component = 'div', className = '' }) => {
+  const Component = component;
+
+  return <Component className={classNames(styles.Typography, styles[variant], className)}>{children}</Component>;
 };
 
 export default Typography;
---
+++ src/components/typography/typography-types.ts
@@ -0,0 +1,16 @@
+import { TypographyVariants } from './Typography';
+
+const TYPOGRAPHY_TYPES = {
+  H1: 'h1',
+  H2: 'h2',
+  H3: 'h3',
+  H4: 'h4',
+  H5: 'h5',
+  H6: 'h6',
+  P1: 'p1',
+  P2: 'p2',
+  P3: 'p3',
+  P4: 'p4'
+} as { [key: string]: TypographyVariants };
+
+export default TYPOGRAPHY_TYPES;

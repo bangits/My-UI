2dd4bdc47851fb148523775f0edd74e558d97b21
rafayelbangits@gmail.com <rafayelbangits@gmail.com>
2021-09-08 19:09:25 +0400

    [ WB-11 ] Added button styles with sass variables

--- src/components/inputs-and-elements/Button/Button.tsx
+++ src/components/inputs-and-elements/Button/Button.tsx
@@ -6,9 +6,39 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ children, onClick }) => (
-  <button onClick={onClick} className={styles.ButtonGhost}>
-    {children}
-  </button>
+  <>
+    <button onClick={onClick} className={`${styles.ButtonBase} ${styles.ButtonGhost}`}>
+      {/* <div className={styles.LeftIcon}>
+        <svg xmlns='http://www.w3.org/2000/svg' width='18' height='18'>
+          <g data-name='Group 111'>
+            <path
+              data-name='Path 15'
+              d='M552 123h-14m0 0 7 7m-7-7 7-7'
+              transform='translate(-536 -114)'
+              style={{ stroke: '#64c9cf', strokeLinecap: 'round', strokeLinejoin: 'round', fill: 'none' }}
+            />
+            <path data-name='Rectangle 677' style={{ fill: 'none' }} d='M0 0h18v18H0z' />
+          </g>
+        </svg>
+      </div> */}
+
+      {children}
+
+      <div className={styles.RightIcon}>
+        <svg xmlns='http://www.w3.org/2000/svg' width='18' height='18'>
+          <g data-name='Group 111'>
+            <path
+              data-name='Path 15'
+              d='M552 123h-14m0 0 7 7m-7-7 7-7'
+              transform='translate(-536 -114)'
+              style={{ stroke: '#64c9cf', strokeLinecap: 'round', strokeLinejoin: 'round', fill: 'none' }}
+            />
+            <path data-name='Rectangle 677' style={{ fill: 'none' }} d='M0 0h18v18H0z' />
+          </g>
+        </svg>
+      </div>
+    </button>
+  </>
 );
 
 export default Button;

06ecd03ac0ebc5298283250e24d697ba5444fedd
ShavarshMartirosyan <shavarshmartirosyan.bangits@gmail.com>
2021-09-25 18:34:41 +0400

    added active and blocked variants in Storybook

--- src/components/Status/Status.stories.tsx
+++ src/components/Status/Status.stories.tsx
@@ -3,11 +3,19 @@ import React from 'react';
 import Status from './Status';
 
 export default {
-    component: Status,
-    decorators: [withKnobs],
-    title: 'components/Status/Status',
-}
+  component: Status,
+  decorators: [withKnobs],
+  title: 'components/Status/Status'
+};
 
 export const Default = () => {
-    return <Status>Active</Status>
-}
+  return (
+    <>
+      <Status variant="active">Active</Status>
+
+      <h1></h1>
+
+      <Status variant='blocked'>Blocked</Status>
+    </>
+  );
+};
--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -1,15 +1,14 @@
 import React, { FC } from 'react';
-import classNames from 'classnames';
 import styles from './Status.module.scss';
 
 export interface StatusProps {
     variant?: "active" | "blocked"
 }
 
-const Status: FC<StatusProps>= ({ children, variant}) => {
+const Status: FC<StatusProps>= ({ children, variant = "active"}) => {
     return (
         <>
-        <div className={styles.StatusBlocked}>
+        <div className={styles[`status--${variant}`]}>
           <span></span>  {children}
         </div>
         </>

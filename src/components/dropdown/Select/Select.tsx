35322f13af73db70ff8dca6b0b04e1e5e0ffef40
rafayelbangits@gmail.com <rafayelbangits@gmail.com>
2021-09-26 11:40:10 +0400

    changed the name of the class

--- src/components/dropdown/Select/Select.tsx
+++ src/components/dropdown/Select/Select.tsx
@@ -1,13 +1,14 @@
 import ReactSelect from '@em/react-select';
 import { StateManagerProps } from '@em/react-select/src/useStateManager';
 import React, { FC } from 'react';
+import styles from './Select.module.scss';
 
 export interface ReactSelectProps {
   selectProps?: StateManagerProps;
 }
 
 const Select: FC<ReactSelectProps> = ({ children, ...selectProps }) => (
-  <ReactSelect className='our__select' classNamePrefix='web__team' {...selectProps} />
+  <ReactSelect className={styles.Select} {...selectProps} />
 );
 
 export default Select;

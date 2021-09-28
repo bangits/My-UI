618a71932dee9c85ae200caa9e014500b44104cb
rafayelbangits@gmail.com <rafayelbangits@gmail.com>
2021-09-28 11:37:12 +0400

    [WB-30] - created select

--- src/components/dropdown/Select/Select.stories.tsx
+++ src/components/dropdown/Select/Select.stories.tsx
@@ -8,4 +8,40 @@ export default {
   title: 'components/Dropdown/Select'
 } as ComponentMeta<typeof Select>;
 
-export const Default = () => <Select isSearchable={true} />;
+export const Default = () => (
+  <Select
+    isSearchable={true}
+    isMulti
+    removeSelected={false}
+    options={[
+      {
+        label: 'Test',
+        value: 1
+      },
+      {
+        label: 'Test2',
+        value: 2
+      },
+      {
+        label: 'Test3',
+        value: 3
+      },
+      {
+        label: 'Test4',
+        value: 4
+      },
+      {
+        label: 'Test5',
+        value: 5
+      },
+      {
+        label: 'Test6',
+        value: 6
+      },
+      {
+        label: 'Test7',
+        value: 7
+      }
+    ]}
+  />
+);
--- src/components/dropdown/Select/Select.tsx
+++ src/components/dropdown/Select/Select.tsx
@@ -1,4 +1,5 @@
-import ReactSelect from '@em/react-select';
+import { Checkbox } from '@/components';
+import ReactSelect, { components } from '@em/react-select';
 import { StateManagerProps } from '@em/react-select/src/useStateManager';
 import React, { FC } from 'react';
 import styles from './Select.module.scss';
@@ -7,8 +8,67 @@ export interface ReactSelectProps {
   selectProps?: StateManagerProps;
 }
 
+const Option = (props) => {
+  return (
+    <div>
+      <components.Option {...props}>
+        <Checkbox checked={props.isSelected} onChange={() => null} /> <label>{props.label}</label>
+      </components.Option>
+    </div>
+  );
+};
+
+const ValueContainer = ({ children, ...props }) => {
+  return (
+    <components.ValueContainer {...props}>
+      <components.Placeholder {...props} isFocused={props.isFocused}>
+        {props.selectProps.placeholder}
+      </components.Placeholder>
+      {React.Children.map(children, (child) =>
+        (child && child.type === components.Input) || components.Placeholder ? child : null
+      )}
+    </components.ValueContainer>
+  );
+};
+
 const Select: FC<ReactSelectProps> = ({ children, ...selectProps }) => (
-  <ReactSelect className={styles.Select} {...selectProps} />
+  <ReactSelect
+    className={styles.Select}
+    styles={{
+      clearIndicator: () => {},
+      container: () => {},
+      control: () => {},
+      dropdownIndicator: () => {},
+      group: () => {},
+      groupHeading: () => {},
+      indicatorsContainer: () => {},
+      indicatorSeparator: () => {},
+      input: () => {},
+      loadingIndicator: () => {},
+      loadingMessage: () => {},
+      menu: () => {},
+      menuList: () => {},
+      menuPortal: () => {},
+      multiValue: () => {},
+      multiValueLabel: () => {},
+      multiValueRemove: () => {},
+      noOptionsMessage: () => {},
+      option: () => {},
+      placeholder: () => {},
+      singleValue: () => {},
+      valueContainer: () => {}
+    }}
+    isClearable={false}
+    hideSelectedOptions={false}
+    closeMenuOnSelect={false}
+    classNamePrefix='react-select'
+    components={{
+      Option
+      // ValueContainer
+    }}
+    isSearchable={false}
+    {...selectProps}
+  />
 );
 
 export default Select;

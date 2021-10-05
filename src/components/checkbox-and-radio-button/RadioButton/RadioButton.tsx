b56f93e6a775431afe4ea93c138bb0c59c0fa62e
ShavarshMartirosyan <shavarshmartirosyan.bangits@gmail.com>
2021-10-05 19:37:41 +0400

    added disabled knob and className

--- src/components/checkbox-and-radio-button/RadioButton/RadioButton.tsx
+++ src/components/checkbox-and-radio-button/RadioButton/RadioButton.tsx
@@ -1,10 +1,11 @@
+import { IComponent, UIColors } from '@/types';
 import classNames from 'classnames';
 import React from 'react';
-import { UIColors } from '../../../types/ui';
 import styles from './RadioButton.module.scss';
 import { Consumer } from './RadioContext';
 
-export interface RadioButtonProps extends React.InputHTMLAttributes<HTMLInputElement> {
+
+export interface RadioButtonProps extends IComponent , React.InputHTMLAttributes<HTMLInputElement> {
   name?: string;
   color?: UIColors;
   value?: string;

1da45ea184aadeaa8855861c3caf965a5fb79165
Vardan Hambardzumyan <vardanhambardzumyan.bangits@gmail.com>
2021-10-21 18:29:13 +0400

    year and month toggle was fixed

--- src/components/dropdowns/DatePicker/DatePickerHeader.tsx
+++ src/components/dropdowns/DatePicker/DatePickerHeader.tsx
@@ -43,12 +43,23 @@ const DatePickerHeader: FC<DatePickerHeaderProps> = ({
         <span className={'react-datepicker__navigation-icon react-datepicker__navigation-icon--previous'}>{'<'}</span>
       </button>
       <div className='month-and-yearWrapper' style={{ display: 'flex', justifyContent: 'center' }}>
-        <span className='react-datepicker__current-month' onClick={() => setMonth(!month)}>
+        <span
+          className='react-datepicker__current-month'
+          onClick={() => {
+            setMonth(true);
+            setYear(false);
+          }}>
           {monthDate.toLocaleString('en-US', {
             month: 'long'
           })}
         </span>
-        <span className='react-datepicker__current-month' onClick={() => setYear(!year)} style={{ marginLeft: '8px' }}>
+        <span
+          className='react-datepicker__current-month'
+          onClick={() => {
+            setMonth(false);
+            setYear(true);
+          }}
+          style={{ marginLeft: '8px' }}>
           {monthDate.toLocaleString('en-US', {
             year: 'numeric'
           })}
@@ -78,7 +89,6 @@ const DatePickerHeader: FC<DatePickerHeaderProps> = ({
             onChange={(year, month) => {
               changeYear(year);
               changeMonth(month);
-              setYear(false);
               setMonth(false);
             }}
             monthDate={monthDate}

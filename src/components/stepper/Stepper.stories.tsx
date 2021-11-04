e8a99c36447f105c572bb82daf2a6a8f490da1cb
ShavarshMartirosyan <shavarshmartirosyan.bangits@gmail.com>
2021-11-04 19:59:08 +0400

    added stepper functionality

--- src/components/stepper/Stepper.stories.tsx
+++ src/components/stepper/Stepper.stories.tsx
@@ -1,6 +1,6 @@
-import { Card } from '@/my-ui-core';
-import { number, withKnobs } from '@storybook/addon-knobs';
-import React from 'react';
+import { Button, Card } from '@/my-ui-core';
+import { object, withKnobs } from '@storybook/addon-knobs';
+import React, { useState } from 'react';
 import Stepper from './Stepper';
 
 export default {
@@ -10,11 +10,38 @@ export default {
 };
 
 export const Default = () => {
+  const [value, setValue] = useState(1);
+
   return (
     <Card borderRadius={1.6} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 81 }}>
+      <Button style={{ display: value < 2 ? 'none' : '' }} variant='ghost' onClick={() => setValue(value - 1)}>
+        Previous
+      </Button>
       <div style={{ width: 582 }}>
-        <Stepper />
+        <Stepper
+          value={value}
+          steps={object('steps', [
+            {
+              title: 'GAME INFORMATION',
+              value: 1
+            },
+            {
+              title: 'GAME PROPERTIES',
+              value: 2
+            },
+            {
+              title: 'OTHER DETAILS',
+              value: 3
+            }
+          ])}
+        />
       </div>
+      <Button disabled={value > 2} variant='ghost' onClick={() => setValue(value + 1)}>
+        Next
+      </Button>
+      <Button style={{ marginLeft: 10 }} onClick={() => setValue(1)}>
+        Reset
+      </Button>
     </Card>
   );
 };
--- src/components/stepper/Stepper.tsx
+++ src/components/stepper/Stepper.tsx
@@ -1,65 +1,66 @@
 import { CheckStepperIcon, EllipseColorIcon, EllipseIcon } from '@/icons';
-import { Card, Typography } from '@/my-ui-core';
+import { Typography } from '@/my-ui-core';
+import { IComponent } from '@/types';
 import classNames from 'classnames';
-import React from 'react';
+import React, { FC, useMemo } from 'react';
 import styles from './Stepper.module.scss';
 
-const Stepper = () => {
+export interface StepperProps extends IComponent {
+  steps?: {
+    title: string;
+    value: number;
+  }[];
+  value?: number;
+}
+
+const Stepper: FC<StepperProps> = ({ steps, value }) => {
+  const activeIndex = useMemo(() => steps.findIndex((o) => o.value === value), [value]);
+
   return (
     <div className={styles.SteeperWrapper}>
-      <div className={classNames(styles.Stepper, styles.StepperChecked)}>
-        <span className={styles.StepperContainer}>
-          <span className={styles.Step}>
-            <div className={styles.StepSection}>
-              <CheckStepperIcon />
+      {steps &&
+        steps.map((option, index) => {
+          return (
+            <div className={classNames(styles.Stepper, styles.StepperChecked)}>
+              {index > 0 && (
+                <div className={styles.StepperLineContainer}>
+                  <span className={styles.StepperLine}></span>
+                </div>
+              )}
+              <span className={styles.StepperContainer}>
+                {activeIndex > index ? (
+                  <span className={styles.Step}>
+                    <div className={styles.StepSection}>
+                      <CheckStepperIcon />
+                    </div>
+                  </span>
+                ) : activeIndex < index ? (
+                  <span className={styles.Step}>
+                    <div className={classNames(styles.StepSection, styles.StepperInactive)}>
+                      <EllipseColorIcon />
+                    </div>
+                  </span>
+                ) : (
+                  <span className={styles.Step}>
+                    <div className={styles.StepSection}>
+                      <EllipseIcon />
+                    </div>
+                  </span>
+                )}
+                <Typography
+                  variant='p5'
+                  component='span'
+                  className={classNames(styles.StepTextContainer, {
+                    [styles.StepTextInactive]: activeIndex < index
+                  })}>
+                  <Typography variant='p5' component='span' className={styles.StepText}>
+                    {option.title}
+                  </Typography>
+                </Typography>
+              </span>
             </div>
-          </span>
-          <Typography variant='p5' component='span' className={styles.StepTextContainer}>
-            <Typography variant='p5' component='span' className={styles.StepText}>
-              GAME INFROMATION
-            </Typography>
-          </Typography>
-        </span>
-      </div>
-
-      <div className={styles.Stepper}>
-        <div className={styles.StepperLineContainer}>
-          <span className={styles.StepperLine}></span>
-        </div>
-        <span className={styles.StepperContainer}>
-          <span className={styles.Step}>
-            <div className={styles.StepSection}>
-              <EllipseIcon />
-            </div>
-          </span>
-          <Typography variant='p5' component='span' className={styles.StepTextContainer}>
-            <Typography variant='p5' component='span' className={styles.StepText}>
-              GAME PROPERTIES
-            </Typography>
-          </Typography>
-        </span>
-      </div>
-
-      <div className={classNames(styles.Stepper, styles.StepperChecked)}>
-        <div className={styles.StepperLineContainer}>
-          <span className={styles.StepperLine}></span>
-        </div>
-        <span className={styles.StepperContainer}>
-          <span className={styles.Step}>
-            <div className={classNames(styles.StepSection, styles.StepperInactive)}>
-              <EllipseColorIcon />
-            </div>
-          </span>
-          <Typography
-            variant='p5'
-            component='span'
-            className={classNames(styles.StepTextContainer, styles.StepTextInactive)}>
-            <Typography variant='p5' component='span' className={styles.StepText}>
-              OTHER DETAILS
-            </Typography>
-          </Typography>
-        </span>
-      </div>
+          );
+        })}
     </div>
   );
 };

7aa4757e790e192d6e09af1e73b26b298358949a
Vardan Hambardzumyan <vardanhambardzumyan.bangits@gmail.com>
2021-11-12 12:58:17 +0400

    avatar dynamic functonality was added

--- src/components/avatar-card/AvatarCard.tsx
+++ src/components/avatar-card/AvatarCard.tsx
@@ -5,35 +5,33 @@ import AvatarMdImg from '../../images/md-avatar.png';
 import AvatarSmImg from '../../images/small-avatar.png';
 import classNames from 'classnames';
 import { IComponent } from '@/types';
-export interface AvatarCardProps extends IComponent {
-  imageSize: string;
-  avatarImg?: string;
-}
-const AvatarCard: FC<AvatarCardProps> = ({ imageSize = 'md', avatarImg }) => {
-  return (
-    <>
-      <div
-        className={classNames({
-          [styles.AvatarMediumCardWrapper]: imageSize === 'md',
-          [styles.AvatarSmallCardWrapper]: imageSize === 'sm'
-        })}>
-        {<img src={avatarImg ? avatarImg : AvatarMdImg} />}
-      </div>
 
+export type AvatarImgSizes = 'md' | 'sm';
+export type AvatarVariant = 'online' | 'offline' | 'default';
+export interface AvatarCardProps extends IComponent {
+  imageSize?: AvatarImgSizes;
+  avatarImg?: string;
+  variant?: AvatarVariant;
+}
+const AvatarCard: FC<AvatarCardProps> = ({ imageSize = 'md', avatarImg, variant = 'online' }) => {
+  return (
+    <div className={styles.AvatarWrapper}>
       <div
-        style={{
-          display: 'flex',
-          width: '10%',
-          justifyContent: 'space-around',
-          alignItems: 'center',
-          marginTop: '3rem'
-        }}>
-        <div className={classNames(styles.AvatarOfflineState, styles['AvatarOnlineState'])}>
-          <img src={AvatarSmImg} />
-          <span className={styles.AvatarStateStatus}></span>
-        </div>
+        className={classNames(
+          {
+            [styles.AvatarMediumCardWrapper]: imageSize === 'md',
+            [styles.AvatarSmallCardWrapper]: imageSize === 'sm'
+          },
+          {
+            [styles.AvatarOfflineState]: variant === 'offline',
+            [styles.AvatarOnlineState]: variant === 'online',
+            [styles.AvatarDefaultState]: variant === 'default'
+          }
+        )}>
+        <img src={avatarImg ? avatarImg : AvatarSmImg} />
+        <span className={styles.AvatarStateStatus}></span>
       </div>
-    </>
+    </div>
   );
 };
 

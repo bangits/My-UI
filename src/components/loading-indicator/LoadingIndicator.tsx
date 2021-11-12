a05894ce7286658a0ca9c0de4e05ff90fdc5b613
rafayelbangits@gmail.com <rafayelbangits@gmail.com>
2021-11-12 19:46:26 +0400

    created LoadingIndicator component

---
+++ src/components/loading-indicator/LoadingIndicator.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './LoadingIndicator.module.scss';
+
+const LoadingIndicator = () => {
+  return <div className={styles.LoadingIndicatorWrapper}></div>;
+};
+
+export default LoadingIndicator;

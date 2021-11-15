4cadfb884aa7cac61d3bbb24397512670ec1db01
rafayelbangits@gmail.com <rafayelbangits@gmail.com>
2021-11-15 17:37:37 +0400

    added variables for loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -5,9 +5,11 @@ import styles from './Loader.module.scss';
 const Loader = () => {
   return (
     <div className={styles.LoadWrapper}>
-      <div className={styles.Loader}></div>
+      <div className={styles.Loader} style={{ margin: '100px auto' }}></div>
     </div>
   );
 };
 
 export default Loader;
+
+//remove style

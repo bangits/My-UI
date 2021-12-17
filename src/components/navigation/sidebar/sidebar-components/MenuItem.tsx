381685acad6bd3cf826c364294a74abe3f20147f
Armen Mesropyan <amesropyan@bangits.com>
2021-12-17 19:17:30 +0400

    Commit for push

--- src/components/navigation/sidebar/sidebar-components/MenuItem.tsx
+++ src/components/navigation/sidebar/sidebar-components/MenuItem.tsx
@@ -1,5 +1,4 @@
 import { typedMemo } from '@/helpers';
-import { HomeSidebar, SidebarArrowBottom, SidebarArrowTop } from '@/icons';
 import { UIColors } from '@/types';
 import { ComponentType, IComponent } from '@/types/props';
 import classNames from 'classnames';
@@ -38,64 +37,25 @@ export const MenuItem: FC<MenuItemProps> = ({
   };
 
   return (
-    <ul className={classNames(styles['ParentList'], 'ParentList')}>
-      {isSidebarOpened ? (
-        <li className={classNames(styles['ParentList__Item'], 'ParentList__Item')}>
-          <a
-            className={classNames(styles['ParentList__Item-Link'], 'ParentList__Item-Link')}
-            onClick={handleArrowClick}>
-            <i className={classNames(styles['ParentCatIcon'], 'ParentCatIcon')}>
-              <HomeSidebar width='15px' />
-            </i>
-            Dashboard
-            <i className={classNames(styles['ArrowTopCell'], 'ArrowTopCell')}>
-              {!isOpen ? <SidebarArrowBottom width='11px' /> : <SidebarArrowTop width='11px' />}
-            </i>
-          </a>
-          <div
-            className={classNames(styles.ChildListCell, {
-              [styles[`ChildListCell--Hidden`]]: !isOpen
-            })}>
-            <div className={classNames(styles['ChildListCell-Height-Detect'], 'ChildListCell-Height-Detect')}>
-              <ul className={classNames(styles['ChildList'], 'ChildList')}>
-                <li className={classNames(styles['ChildList__Item'], 'ChildList__Item')}>
-                  <a className={classNames(styles['ChildList__Item-Link'], 'ChildList__Item-Link')}>Lorem Ipsum</a>
-                </li>
-                <li className={classNames(styles['ChildList__Item'], 'ChildList__Item')}>
-                  <a className={classNames(styles['ChildList__Item-Link'], 'ChildList__Item-Link')}>Lorem Ipsum</a>
-                </li>
-                <li className={classNames(styles['ChildList__Item'], 'ChildList__Item')}>
-                  <a className={classNames(styles['ChildList__Item-Link'], 'ChildList__Item-Link')}>Lorem Ipsum</a>
-                </li>
-                <li className={classNames(styles['ChildList__Item'], 'ChildList__Item')}>
-                  <a className={classNames(styles['ChildList__Item-Link'], 'ChildList__Item-Link')}>Lorem Ipsum</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </li>
-      ) : (
-        <Component
-          {...menuItemProps}
-          className={classNames(
-            styles.MenuItemBase,
-            {
-              [styles[`MenuItemBase--${color}`]]: color,
-              [styles['MenuItemBase--isActive']]: isActive
-            },
-            menuItemProps.className
-          )}>
-          <div className={classNames(styles['MenuItemBase--icon'])}>{icon}</div>
-          <a className={classNames(styles['MenuItemBase--label'])}>{label}</a>
-          <div className={classNames({ [styles['MenuItemBase--sub']]: subItems?.length > 0 })}>
-            <p className={classNames([styles['MenuItemBase--sub-header']])}>{label}</p>
-            {subItems?.map((item, key) => (
-              <SubMenuItems key={key} label={item.label} onClick={item.onClick} />
-            ))}
-          </div>
-        </Component>
-      )}
-    </ul>
+    <Component
+      {...menuItemProps}
+      className={classNames(
+        styles.MenuItemBase,
+        {
+          [styles[`MenuItemBase--${color}`]]: color,
+          [styles['MenuItemBase--isActive']]: isActive
+        },
+        menuItemProps.className
+      )}>
+      <div className={classNames(styles['MenuItemBase--icon'])}>{icon}</div>
+      <a className={classNames(styles['MenuItemBase--label'])}>{label}</a>
+      <div className={classNames({ [styles['MenuItemBase--sub']]: subItems?.length > 0 })}>
+        <p className={classNames([styles['MenuItemBase--sub-header']])}>{label}</p>
+        {subItems?.map((item, key) => (
+          <SubMenuItems key={key} label={item.label} onClick={item.onClick} />
+        ))}
+      </div>
+    </Component>
   );
 };
 

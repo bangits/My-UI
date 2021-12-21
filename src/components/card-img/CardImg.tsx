9a1aed1347b1c3b3085d65e422c076ebb69a729c
Hayk Ghazaryan <hghazaryan@bangits.com>
2021-12-21 06:48:43 +0400

    CardImg add props hoverComponent

--- src/components/card-img/CardImg.tsx
+++ src/components/card-img/CardImg.tsx
@@ -1,37 +1,23 @@
+import React, { FC, ReactNode } from 'react';
+import classNames from 'classnames';
 import { Typography } from '@/my-ui-core';
 import { IComponent } from '@/types';
-import React, { FC } from 'react';
 import styles from './CardImg.module.scss';
-import { ViewCardImageIcon, PlayArrowIcon, PhotoCamIcon } from '@/icons';
-import classNames from 'classnames';
 export interface CardImgProps extends IComponent {
   title: string;
   image?: string;
+  hoverComponent?: ReactNode;
   handleClick?: () => void;
 }
-const CardImg: FC<CardImgProps> = ({ title, image, handleClick }) => {
+const CardImg: FC<CardImgProps> = ({ title, image, handleClick, hoverComponent }) => {
   return (
-    <div className={styles.CardImgContainer} onClick={handleClick}>
-      <div className={styles.CardImg}>
-        {/* <span className={classNames(styles['HoverBox'], 'HoverBox')}>
-          <span className={classNames(styles['HoverBox-PlayBtn'], 'HoverBox-PlayBtn')}>
-            <span className={classNames(styles['HoverBox-PlayBtnInner'], 'HoverBox-PlayBtnInner')}>
-              <span className={classNames(styles['HoverBox-PlayBtnIcon'], 'HoverBox-PlayBtnIcon')}>
-                <PlayArrowIcon width={'100%'} />
-              </span>
-            </span>
-          </span>
-          <span className={classNames(styles['HoverBox-ViewIcon'], 'HoverBox-ViewIcon')}>
-            <ViewCardImageIcon width={'100%'} />
-          </span>
-          <span className={classNames(styles['HoverBox-LinkText'], 'HoverBox-LinkText')}>Play demo</span>
-          <span className={classNames(styles['HoverBox-OpacityLayer'], 'HoverBox-OpacityLayer')}></span>
-        </span> */}
-
+    <div className={classNames(styles['CardImgContainer'], 'CardImgContainer')} onClick={handleClick}>
+      <div className={classNames(styles['CardImg'], 'CardImg')}>
+        {hoverComponent}
         {image && <img src={image} alt='' />}
       </div>
-      <div className={styles.CardTextContainer}>
-        <Typography variant='p5' component='p' className={styles.CardText}>
+      <div className={classNames(styles['CardTextContainer'], 'CardTextContainer')}>
+        <Typography className={classNames(styles['CardText'], 'CardText')} variant='p5' component='p'>
           {title}
         </Typography>
       </div>

0f12c648604ed2be5f1a863c4834ba84408f74c6
Hayk Ghazaryan <hghazaryan@bangits.com>
2021-12-21 07:35:07 +0400

    CardImg move event onClick from image to on title

--- src/components/card-img/CardImg.tsx
+++ src/components/card-img/CardImg.tsx
@@ -11,14 +11,14 @@ export interface CardImgProps extends IComponent {
 }
 const CardImg: FC<CardImgProps> = ({ title, image, handleClick, hoverComponent }) => {
   return (
-    <div className={classNames(styles['CardImgContainer'], 'CardImgContainer')} onClick={handleClick}>
+    <div className={classNames(styles['CardImgContainer'], 'CardImgContainer')}>
       <div className={classNames(styles['CardImg'], 'CardImg')}>
         {hoverComponent}
         {image && <img src={image} alt='' />}
       </div>
       <div className={classNames(styles['CardTextContainer'], 'CardTextContainer')}>
         <Typography className={classNames(styles['CardText'], 'CardText')} variant='p5' component='p'>
-          {title}
+          <span onClick={handleClick}>{title}</span>
         </Typography>
       </div>
     </div>

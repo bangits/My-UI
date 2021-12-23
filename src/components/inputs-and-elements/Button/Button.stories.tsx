fe763f70e9064dcdee9daf5911d7e9376affa228
Hayk Ghazaryan <hghazaryan@bangits.com>
2021-12-23 19:22:36 +0400

    currentColorF

--- src/components/inputs-and-elements/Button/Button.stories.tsx
+++ src/components/inputs-and-elements/Button/Button.stories.tsx
@@ -51,11 +51,8 @@ export const WithIcons = () => (
       variant={optionsKnob('variant', BUTTON_TYPES, BUTTON_TYPES.ghost, { display: 'inline-radio' })}
       disabled={boolean('disabled', false)}
       endIcon={
-        <svg xmlns='http://www.w3.org/2000/svg' width='16' viewBox='0 0 10 10'>
-          <path
-            style={{ fill: 'currentColor' }}
-            d='M5 10c-.1 0-.3 0-.4-.1-.2-.2-.2-.5 0-.7l3.6-3.6H.4c-.3 0-.5-.2-.5-.5s.2-.5.5-.5h7.8L4.6 1c-.2-.2-.2-.5 0-.7.2-.2.5-.2.7 0l4.5 4.5.1.1v.4c0 .1-.1.1-.1.1L5.3 9.9s-.2.1-.3.1z'
-          />
+        <svg xmlns='http://www.w3.org/2000/svg' width='1.6rem' viewBox='0 0 10 10' fill='currentColor'>
+          <path d='M5 10c-.1 0-.3 0-.4-.1-.2-.2-.2-.5 0-.7l3.6-3.6H.4c-.3 0-.5-.2-.5-.5s.2-.5.5-.5h7.8L4.6 1c-.2-.2-.2-.5 0-.7.2-.2.5-.2.7 0l4.5 4.5.1.1v.4c0 .1-.1.1-.1.1L5.3 9.9s-.2.1-.3.1z' />
         </svg>
 
         // <svg xmlns='http://www.w3.org/2000/svg' width='18' height='18' style={{ transform: 'rotate(180deg)' }}>
@@ -81,7 +78,7 @@ export const WithIcons = () => (
       disabled={boolean('disabled', false)}
       startIcon={
         <>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8.1 8.1' width='16' style={{ fill: 'currentColor' }}>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8.1 8.1' width='1.6rem' fill='currentColor'>
             <path d='M4 8.1s-.1 0-.2-.1L.1 4.3 0 4.2V4s0-.1.1-.1L3.8.2c.1-.2.4-.2.5-.1s.1.4 0 .5l-3 3.1h6.5c.2 0 .4.2.4.4s-.3.3-.5.3H1.3l3.1 3.1c.1.1.1.4 0 .5-.2.1-.3.1-.4.1z' />
           </svg>
 
@@ -109,7 +106,7 @@ export const WithIcons = () => (
       disabled={boolean('disabled', false)}
       startIcon={
         <>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 13.1 12.8' width='16' fill='currentColor'>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 13.1 12.8' width='1.6rem' fill='currentColor'>
             <path d='M8.3 2.1c-.1-.1-.3-.2-.4-.2-.1 0-.2.1-.3.1L1 8.7c-.1 0-.2.2-.2.3L0 11.9c-.1.4.1.8.6.9H1l2.9-.7c.1 0 .3-.1.4-.2l6.6-6.6c.2-.2.2-.5 0-.7L8.3 2.1zm-4.7 9.1-2.5.6.6-2.5 6.2-6.2L9.8 5l-6.2 6.2z' />
             <path d='M12.5 1.2c0-.1 0-.1 0 0l-.7-.7c-.7-.7-1.8-.7-2.5 0l-1 .8c-.1.1-.2.2-.2.4s.1.2.2.3l2.6 2.6c.1.1.2.1.4.1s.3 0 .4-.1l.9-.9c.6-.7.6-1.8-.1-2.5zM11.8 3l-.5.5-1.9-1.8.5-.5c.3-.3.8-.3 1.1 0l.8.7c.3.3.3.8 0 1.1z' />
           </svg>
@@ -144,7 +141,7 @@ export const WithIcons = () => (
       disabled={boolean('disabled', false)}
       startIcon={
         <>
-          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8.1 8.1' width='16' style={{ fill: 'currentColor' }}>
+          <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8.1 8.1' width='1.6rem' fill='currentColor'>
             <path d='M4 8.1s-.1 0-.2-.1L.1 4.3 0 4.2V4s0-.1.1-.1L3.8.2c.1-.2.4-.2.5-.1s.1.4 0 .5l-3 3.1h6.5c.2 0 .4.2.4.4s-.3.3-.5.3H1.3l3.1 3.1c.1.1.1.4 0 .5-.2.1-.3.1-.4.1z' />
           </svg>
 

efafc2b15c78e0757ba7e18446bef7a1e1a8257f
rafayelbangits@gmail.com <rafayelbangits@gmail.com>
2022-02-21 11:01:59 +0400

    commit for checkout branch

--- src/components/typography/Typography.tsx
+++ src/components/typography/Typography.tsx
@@ -1,22 +1,22 @@
 import { ComponentType, IComponent, UIColors } from '@/types';
 import classNames from 'classnames';
-import { forwardRef, ReactNode } from 'react';
+import { forwardRef, HTMLAttributes, ReactNode } from 'react';
 import styles from './Typography.module.scss';
 
 export type TypographyVariants = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p1' | 'p2' | 'p3' | 'p4' | 'p5';
 
-export interface TypographyProps extends IComponent {
+export interface TypographyProps extends IComponent, HTMLAttributes<HTMLOrSVGElement> {
   variant?: TypographyVariants;
   component?: ComponentType;
   color?: UIColors;
   children: ReactNode;
 }
 
-const Typography = forwardRef<HTMLElement, TypographyProps>(
+const Typography = forwardRef<any, TypographyProps>(
   ({ children, variant = 'p1', component: Component = 'div', className, color }, ref) => {
     return (
-      // @ts-expect-error Ignoring typescript for ref typecast
       <Component
+        // @ts-expect-error Ignoring typescript for union typecast
         className={classNames(
           styles.Typography,
           styles[variant],

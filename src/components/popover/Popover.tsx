184a333d0c186e7927f554d441f166e9d5be681f
Gor Sargsyan <g.sargsyan@bangits.com>
2023-04-14 13:25:57 +0400

    feat: added logic to prevent overflows,resize logic added to keep right position on page size change

--- src/components/popover/Popover.tsx
+++ src/components/popover/Popover.tsx
@@ -1,6 +1,7 @@
 import { ReactNode, useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import classes from './Popover.module.scss';
 import { AlignemntHorizontal, AlignmentVertical } from './enums';
+import classes from './Popover.module.scss';
+
 import {
   getHorizontalTranslate,
   getLeftPosition,
@@ -11,7 +12,6 @@ import {
   hasRightMargin,
   hasTopMargin
 } from './helpers';
-
 export interface Origins {
   vertical: AlignmentVertical;
   horizontal: AlignemntHorizontal;
@@ -27,6 +27,7 @@ export interface PopoverProps {
   };
   anchorEl?: HTMLElement;
   edgeMarginUnit?: number;
+  safetyMarginUnit?: number;
   anchorOrigin?: Origins;
   transformOrigin?: Origins;
   onClose: () => void;
@@ -38,28 +39,84 @@ const Popover = ({
   onClose,
   anchorEl,
   edgeMarginUnit = 4,
-  anchorOrigin = { vertical: AlignmentVertical.top, horizontal: AlignemntHorizontal.right },
-  transformOrigin = { vertical: AlignmentVertical.bottom, horizontal: AlignemntHorizontal.right }
+  safetyMarginUnit = 24,
+  anchorOrigin = { vertical: AlignmentVertical.bottom, horizontal: AlignemntHorizontal.left },
+  transformOrigin = { vertical: AlignmentVertical.top, horizontal: AlignemntHorizontal.left }
 }: PopoverProps) => {
+  const [contentRects, setContentRects] = useState<any>(null);
+  const [containerRects, setContainerRects] = useState<any>(null);
   const [originPosition, setOriginPosition] = useState<any>({});
   const [anchorPosition, setAnchorPosition] = useState<any>({});
-  const [activeContainerRef, setActiveContainerRef] = useState<HTMLDivElement | null>(null);
-  const [activeContentRef, setActiveContentRef] = useState<HTMLDivElement | null>(null);
 
-  const contentRef = useRef();
-  const containertRef = useRef();
+  const contentRef = useRef<HTMLDivElement | null>(null);
+  const containertRef = useRef<HTMLDivElement | null>(null);
 
-  const hanldeClose = () => onClose();
+  const positionStyles = useMemo(() => {
+    if (!contentRects || !containerRects) {
+      return null;
+    }
+
+    const current: any = {
+      top: anchorPosition.top - originPosition.top,
+      left: anchorPosition.left - originPosition.left
+    };
+
+    if (current.left < safetyMarginUnit) {
+      current.left = safetyMarginUnit;
+    }
+
+    if (current.top < safetyMarginUnit) {
+      current.top = safetyMarginUnit;
+    }
+
+    if (contentRects.width >= containerRects.width) {
+      current.left = safetyMarginUnit;
+    }
+
+    if (contentRects.height >= containerRects.height) {
+      current.top = safetyMarginUnit;
+    }
+
+    if (contentRects.width + current.left > containerRects.width - safetyMarginUnit) {
+      current.right = safetyMarginUnit;
+      delete current.left;
+    }
+
+    if (contentRects.height + current.top > containerRects.height - safetyMarginUnit) {
+      current.bottom = safetyMarginUnit;
+      delete current.top;
+    }
+
+    return current;
+  }, [anchorPosition, originPosition, contentRects, containerRects, safetyMarginUnit]);
+
+  const edgeMargins = useMemo(() => {
+    const top = hasTopMargin(anchorOrigin, transformOrigin);
+    const right = hasRightMargin(anchorOrigin, transformOrigin);
+    const left = hasLeftMargin(anchorOrigin, transformOrigin);
+    const bottom = hasRightBottom(anchorOrigin, transformOrigin);
+
+    return {
+      top: top && edgeMarginUnit,
+      left: right && edgeMarginUnit,
+      right: left && edgeMarginUnit,
+      bottom: bottom && edgeMarginUnit
+    };
+  }, [anchorOrigin, transformOrigin, edgeMarginUnit]);
+
+  const hanldeClose = () => {
+    onClose();
+    setContentRects(null);
+    setContainerRects(null);
+  };
   const blockClose = (e: React.SyntheticEvent) => e.stopPropagation();
 
   const updateOriginPosition = useCallback(() => {
-    const rects = activeContentRef?.getBoundingClientRect();
-
     setOriginPosition({
-      top: getVerticalTranslate(rects, transformOrigin.vertical),
-      left: getHorizontalTranslate(rects, transformOrigin.horizontal)
+      top: getVerticalTranslate(contentRects, transformOrigin.vertical),
+      left: getHorizontalTranslate(contentRects, transformOrigin.horizontal)
     });
-  }, [transformOrigin.vertical, transformOrigin.horizontal, activeContentRef]);
+  }, [transformOrigin.vertical, transformOrigin.horizontal, contentRects]);
 
   const updateAnchorPosition = useCallback(() => {
     const anchorRects = anchorEl?.getBoundingClientRect();
@@ -71,62 +128,32 @@ const Popover = ({
   }, [anchorEl, anchorOrigin.horizontal, anchorOrigin.vertical]);
 
   const updateContentRef = useCallback(
-    () => (open ? setActiveContentRef(contentRef.current) : setActiveContentRef(null)),
+    () => contentRef.current && setContentRects(contentRef.current?.getBoundingClientRect()),
     [open]
   );
 
   const updateContainerRef = useCallback(
-    () => (open ? setActiveContainerRef(containertRef.current) : setActiveContainerRef(null)),
+    () => containertRef.current && setContainerRects(containertRef.current?.getBoundingClientRect()),
     [open]
   );
 
-  const positionStyles = useMemo(() => {
-    const contentRects = activeContentRef?.getBoundingClientRect();
-    const containerRects = activeContainerRef?.getBoundingClientRect();
+  const cb = () => {
+    const rects = containertRef?.current?.getBoundingClientRect();
+    rects && setContainerRects(rects);
+  };
 
-    if (!anchorPosition || !originPosition || !contentRects || !containerRects) {
-      return;
-    }
+  const registerResizeHandler = useCallback(() => {
+    window.addEventListener('resize', cb);
+  }, []);
 
-    const currentPosition: any = {
-      top: anchorPosition.top - originPosition.top,
-      left: anchorPosition.left - originPosition.left
-    };
+  const unRegisterResizeHandler = useCallback(() => {
+    window.removeEventListener('resize', cb);
+  }, []);
 
-    if (currentPosition.top < 24) {
-      currentPosition.top = 24;
-    }
+  useEffect(() => {
+    open ? registerResizeHandler() : unRegisterResizeHandler();
+  }, [open]);
 
-    if (currentPosition.left < 24) {
-      currentPosition.left = 24;
-    }
-
-    if (contentRects.right - containerRects.right < 24) {
-      currentPosition.right = 24;
-    }
-
-    if (contentRects.bottom - containerRects.bottom > 24) {
-      currentPosition.bottom = 24;
-    }
-
-    return currentPosition;
-  }, [anchorPosition, originPosition]);
-
-  const edgeMargins = useMemo(() => {
-    const unit = edgeMarginUnit;
-
-    const top = hasTopMargin(anchorOrigin, transformOrigin);
-    const right = hasRightMargin(anchorOrigin, transformOrigin);
-    const left = hasLeftMargin(anchorOrigin, transformOrigin);
-    const bottom = hasRightBottom(anchorOrigin, transformOrigin);
-
-    return {
-      top: top && unit,
-      left: right && unit,
-      right: left && unit,
-      bottom: bottom && unit
-    };
-  }, [anchorOrigin, transformOrigin]);
   useEffect(() => updateOriginPosition(), [updateOriginPosition]);
 
   useEffect(() => updateAnchorPosition(), [updateAnchorPosition]);
@@ -136,21 +163,19 @@ const Popover = ({
   useEffect(() => updateContainerRef(), [updateContainerRef]);
 
   return (
-    <div>
-      {open && (
-        <div
-          ref={containertRef}
-          style={{ opacity: activeContentRef ? '1' : '0' }}
-          onClick={hanldeClose}
-          className={`${classes.fullWidthHeightLayer}`}>
-          <div className={`${classes.cardWrapper}`} style={edgeMargins}>
-            <div ref={contentRef} onClick={blockClose} style={{ ...positionStyles }} className={`${classes.cardBase}`}>
-              {children}
-            </div>
+    open && (
+      <div
+        ref={containertRef}
+        style={{ visibility: contentRects ? 'visible' : 'hidden' }}
+        onClick={hanldeClose}
+        className={`${classes.fullWidthHeightLayer}`}>
+        <div className={`${classes.cardWrapper}`} style={edgeMargins}>
+          <div ref={contentRef} onClick={blockClose} className={`${classes.cardBase}`} style={positionStyles}>
+            {children}
           </div>
         </div>
-      )}
-    </div>
+      </div>
+    )
   );
 };
 

368b5704cac1e9478f1a906ae7fc6b41ba460e41
Gor Sargsyan <g.sargsyan@bangits.com>
2023-06-09 12:43:46 +0400

    feat: added action icon to tag with image component

--- src/components/tag-with-image/TagWithImage.stories.tsx
+++ src/components/tag-with-image/TagWithImage.stories.tsx
@@ -1,5 +1,6 @@
 import { text, withKnobs } from '@storybook/addon-knobs';
 import TagWithImage from './TagWithImage';
+import { Icons } from '@/my-ui-core';
 
 export default {
   component: <></>,
@@ -10,8 +11,9 @@ export default {
 export const Default = () => {
   return (
     <TagWithImage
+      showActionIcon
       label={text('label', 'Armenia')}
-      imagePosition='end'
+      imagePosition='start'
       imageSource={text(
         'source',
         'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Flag_of_Armenia.svg/1024px-Flag_of_Armenia.svg.png'
--- src/components/tag-with-image/TagWithImage.tsx
+++ src/components/tag-with-image/TagWithImage.tsx
@@ -1,4 +1,5 @@
 import { DetailedHTMLProps, FC, HTMLAttributes, ReactNode, useMemo } from 'react';
+import { AlertClose } from '@/icons';
 import { Typography } from '@/my-ui-core';
 import classNames from 'classnames';
 import styles from './TagWithImage.module.scss';
@@ -7,12 +8,25 @@ export interface TagWithImageBaseProps {
   label: ReactNode;
   imageSource: string;
   imagePosition?: 'start' | 'end';
+  actionIcon?: React.FC<React.SVGProps<SVGSVGElement>>;
+  showActionIcon?: boolean;
+  onActionIconClick?: () => void;
+  onClick?: () => void;
   className?: string;
 }
 
 export type TagWithImageProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, null> & TagWithImageBaseProps;
 
-const TagWithImage: FC<TagWithImageProps> = ({ label, imageSource, className, imagePosition = 'start' }) => {
+const TagWithImage: FC<TagWithImageProps> = ({
+  label,
+  imageSource,
+  className,
+  imagePosition = 'start',
+  actionIcon: ActionIcon = AlertClose,
+  showActionIcon,
+  onActionIconClick,
+  onClick
+}) => {
   const labelClasses = useMemo(
     () =>
       classNames(styles['Label'], {
@@ -22,22 +36,47 @@ const TagWithImage: FC<TagWithImageProps> = ({ label, imageSource, className, im
     [imagePosition]
   );
 
+  const ActionIconComponent = () => (
+    <ActionIcon
+      width={10}
+      height={10}
+      className={classNames(styles['ActionIcon'], {
+        [styles['ActionIcon--left-margin']]: imagePosition === 'start',
+        [styles['ActionIcon--right-margin']]: imagePosition === 'end'
+      })}
+      onClick={(e) => {
+        e.stopPropagation();
+        onActionIconClick?.();
+      }}
+    />
+  );
+
   return (
-    <div className={classNames(styles['TagImageWrapper'], className)}>
-      {imagePosition === 'start' && (
+    <div
+      className={classNames(
+        styles['TagImageWrapper'],
+        { [styles['TagImageWrapper--hover']]: showActionIcon },
+        className
+      )}
+      onClick={() => onClick?.()}>
+      {imagePosition === 'start' ? (
         <div className={styles.Image}>
           <img src={imageSource} />
         </div>
+      ) : (
+        showActionIcon && <ActionIconComponent />
       )}
       <div className={labelClasses}>
         <Typography component='span' variant='p4'>
           {label}
         </Typography>
       </div>
-      {imagePosition === 'end' && (
+      {imagePosition === 'end' ? (
         <div className={styles.Image}>
           <img src={imageSource} />
         </div>
+      ) : (
+        showActionIcon && <ActionIconComponent />
       )}
     </div>
   );

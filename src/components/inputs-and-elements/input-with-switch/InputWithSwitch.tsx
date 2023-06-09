47db816f5b19b8b946742545369a3f66a4194ae8
Gor Sargsyan <g.sargsyan@bangits.com>
2023-06-09 13:11:59 +0400

    fix: fixed label issue at input with switch component

--- src/components/inputs-and-elements/input-with-switch/InputWithSwitch.tsx
+++ src/components/inputs-and-elements/input-with-switch/InputWithSwitch.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, useCallback, MouseEvent, useMemo } from 'react';
-import { TextInput, TextInputProps } from '../TextInput';
+import { ChangeEvent, useCallback, MouseEvent, useMemo, useState } from 'react';
 import { SwitchForInput, SwitchForInputProps } from './switch-for-input/SwitchForInput';
+import { TextInput, TextInputProps } from '../TextInput';
 
 export interface InputWithSwitchProps {
   inputProps?: TextInputProps;
@@ -21,14 +21,32 @@ export const InputWithSwitch = ({
   switchProps,
   onSwitchChange
 }: InputWithSwitchProps) => {
-  const handleInputChange = useCallback((e) => onInputChange?.(e.target.value, e), [onInputChange]);
+  const [isFocused, setIsFocused] = useState(false);
 
+  const handleInputChange = useCallback((e) => onInputChange?.(e.target.value, e), [onInputChange]);
   const handleSwitchChange = useCallback((value, e) => onSwitchChange?.(value, e), [onSwitchChange]);
 
   const initialOptionIndex = useMemo(() => {
     const optionIndex = switchOptions.findIndex((option) => option.id === switchValue);
     return optionIndex === -1 ? 0 : optionIndex;
   }, [switchOptions, switchValue]);
+
+  const label = useMemo(
+    () =>
+      inputProps.label ? (
+        <div
+          style={{
+            width: '100%',
+            textOverflow: 'ellipsis',
+            boxSizing: 'border-box',
+            overflow: 'hidden',
+            paddingRight: isFocused ? 0 : '4rem'
+          }}>
+          {inputProps?.label}
+        </div>
+      ) : null,
+    [inputProps, isFocused]
+  );
 
   return (
     <div>
@@ -44,6 +62,9 @@ export const InputWithSwitch = ({
           />
         }
         {...inputProps}
+        label={label}
+        onBlur={() => setIsFocused(false)}
+        onFocus={() => setIsFocused(true)}
         onChange={handleInputChange}
         value={inputValue}
       />

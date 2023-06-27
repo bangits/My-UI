59bf9c8632af8b34a92adc1156f18900fcc2c249
MiqayelGalstyanBangits <m.galstyan@bangits.com>
2023-06-27 11:15:04 +0400

    added input with color picker

---
+++ src/components/color-picker/ColorPicker.stories.tsx
@@ -0,0 +1,22 @@
+import { withKnobs } from '@storybook/addon-knobs';
+import { useRef, useState } from 'react';
+import ColorPicker from './ColorPicker';
+
+export default {
+  component: ColorPicker,
+  decorators: [withKnobs],
+  title: 'components/ColorPicker/ColorPicker'
+};
+
+const defaultColor = 'black';
+
+export const Default = () => {
+  const [value, setValue] = useState(defaultColor);
+  const pickerRef = useRef(null);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value)
+  };
+
+  return <ColorPicker value={value} pickerRef={pickerRef} onChange={handleChange} />;
+};
--- src/components/inputs-and-elements/index.ts
+++ src/components/inputs-and-elements/index.ts
@@ -3,3 +3,4 @@ export * from './IconButton';
 export * from './input-with-dropdown';
 export * from './input-with-switch';
 export * from './TextInput';
+export * from './input-with-color-picker';
